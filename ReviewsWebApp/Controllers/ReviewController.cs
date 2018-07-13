e4effe65cb0d9b525d070006974f00d505321123
dmongirulli_iglu <davide.mongirulli@iglu.com>
2018-07-13 14:45:59 +0100

    first commit

---
+++ ReviewsWebApp/Controllers/ReviewController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace ReviewsWebApp.Controllers
+{
+    public class ReviewController : Controller
+    {
+        // GET: Review
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
